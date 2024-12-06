d6ea857d74a76fd80ccbb28dddab5294bb9840c4
gzz <zzguo2052110@outlook.com>
2024-12-06 10:59:57 +0800

    add autosave when esc insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,3 +11,23 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
     vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode", buffer = buf, nowait = true })
   end,
 })
+
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+  save()
+  end,
+  pattern = "*",
+  group = "AutoSave",
+})
