b656bc2a6a35eb1386e99ced656c4e8387a6b58d
gzz <zzguo2052110@outlook.com>
2025-04-10 14:46:44 +0800

    change terminal setting
    - remove <esc><esc> autocmd due to default settings change
    - disable start_insert & auto_insert for snacks.nvim terminal

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,18 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- terminal double escape to normal mode
-vim.api.nvim_create_autocmd({ "TermOpen" }, {
-  group = nativeTermGroup,
-  pattern = "term://*",
-  callback = function(event)
-    local buf = event.buf
-    -- vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window", buffer = buf, nowait = true })
-    -- vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to Above Window", buffer = buf, nowait = true })
-    vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode", buffer = buf, nowait = true })
-  end,
-})
-
 -- auto save buffer when escaping insert mode
 local function save()
   local buf = vim.api.nvim_get_current_buf()
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,11 +2,13 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-       terminal = {
-         win = {
-           position = "float",
-         },
-       },
+      terminal = {
+        win = {
+          position = "float",
+        },
+        start_insert = false,
+        auto_insert = false,
+      },
     },
   },
 }
