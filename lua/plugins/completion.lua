78f2a41a63baaf6b10bcf2c270144387d6093d27
gzz <zzguo2052110@outlook.com>
2025-05-28 14:07:40 +0800

    [feat(snippets)] add markdown snippets, set Tab as accept keymap

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    'saghen/blink.cmp',
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      completion = {
+        documentation = {
+          auto_show = true,
+        }
+      },
+      sources = {
+        providers = {
+          snippets = {score_offset = 1000},
+        }
+      },
+      keymap = {
+        preset = "default",
+        ['<Tab>'] = {
+          'snippet_forward',
+          function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+          end,
+          'fallback'
+        },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      }
+    }
+  }
+}
