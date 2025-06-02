3cf41f576e31a6f90c16d5217f530ef2e4d2a57c
gzz <zzguo2052110@outlook.com>
2025-06-02 16:16:51 +0800

    [feat(flash)] disable all keymap except 's'

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,11 +5,10 @@ return {
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "S", mode = { "v" }, false },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "S", mode = { "n", "x", "o", "v" }, false },
+      { "r", mode = { "n", "x", "o", "v" }, false },
+      { "R", mode = { "n", "x", "o", "v" }, false },
+      { "<c-s>", mode = { "n", "x", "o", "v" }, false },
     },
   }
 }
