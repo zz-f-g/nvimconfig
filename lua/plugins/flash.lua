f3206cf9e55d6e3d426a66fff9bb3c499d6bb8f3
gzz <zzguo2052110@outlook.com>
2025-06-02 16:09:42 +0800

    [chore(flash)]

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -6,7 +6,7 @@ return {
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "S", mode = "v", false },
+      { "S", mode = { "v" }, false },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
