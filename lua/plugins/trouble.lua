696586b2faff7fec2d98cd8aa57405c602ce69d7
gzz <zzguo2052110@outlook.com>
2025-06-02 16:08:58 +0800

    [feat(trouble)] open trouble symbols auto focus

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=true<cr>",
+        desc = "Symbols (Trouble)",
+      },
+    },
+  }
+}
