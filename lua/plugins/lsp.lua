d2c1552ff551f70d921556a01570495640b26e7f
gzz <zzguo2052110@outlook.com>
2025-06-02 16:07:54 +0800

    [factor(lsp)] move lsp config into one file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       servers = {
         basedpyright = { -- pyright but based. manually install off PyPI
+          mason = false,
           settings = {
             basedpyright = {
               analysis = {
@@ -38,6 +39,42 @@ return {
                 },
               },
             },
+          },
+        },
+        clangd = {
+          mason = false,
+          keys = {
+            { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja"
+            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+              fname
+            ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
           },
         },
       },
