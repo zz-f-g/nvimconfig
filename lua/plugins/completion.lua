c616ed866facfd05713b705cce66f611d0c69566
gzz <zzguo2052110@outlook.com>
2025-10-08 18:52:02 +0800

    feat(completion): use default prior

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,21 +8,21 @@ return {
           auto_show = true,
         }
       },
-      sources = {
-        providers = {
-          snippets = {score_offset = 1000},
-        }
-      },
+      -- sources = {
+      --   providers = {
+      --     snippets = {score_offset = 1000},
+      --   }
+      -- },
       keymap = {
         preset = "default",
         ['<Tab>'] = {
           'snippet_forward',
           function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
           end,
           'fallback'
         },
