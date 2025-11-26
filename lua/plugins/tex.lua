bfc0c917fa76a9234001f77b1637ddc5149a3484
gzz <zzguo2052110@outlook.com>
2025-11-26 18:47:04 +0800

    feat(tex): change keymap to default

--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -5,7 +5,7 @@ return {
     init = function ()
       vim.g.vimtex_view_method = "sioyek"
       vim.g.vimtex_view_sioyek_exe = "/opt/sioyek/Sioyek-x86_64.AppImage"
-      vim.g.vimtex_mappings_prefix = "<leader>t"
+      -- vim.g.vimtex_mappings_prefix = "<leader>t"
     end,
   }
 }
