10d0ba42b60c00681819c925e6fb6e2a31c1d923
gzz <zzguo2052110@outlook.com>
2025-12-31 13:37:28 +0800

    feat(treesitter): disable treesitter plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "mason-org/mason.nvim", enabled = false, },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
 }
